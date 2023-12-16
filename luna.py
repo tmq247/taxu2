ab237fcc8bf8f37286ec4a4c81056925c658d3a4
tmq247 <60788746+tmq247@users.noreply.github.com>
2023-12-17 02:05:58 +0700

    Update luna.py

--- luna.py
+++ luna.py
@@ -350,7 +350,7 @@ Tổng thua: {total_bet_T + total_bet_X - total_win:,}đ
 @bot.on_message(filters.command("diem"))
 async def check_balance(_, message: Message):
     load_balance_from_file()
-    user_id = await extract_user(message)#
+    user_id, username = await extract_user_and_reason(message)#
     user = await app.get_users(user_id)#
     from_user = message.from_user#
     if not user_id: #message.reply_to_message:
@@ -371,7 +371,7 @@ async def check_balance(_, message: Message):
         #if user_id not in user_balance:
             #return await bot.send_message(message.chat.id, f"{mention} chưa khởi động bot. Vui lòng khởi động bot.")
         #await bot.send_message(message.chat.id, f"👤 Số điểm của {message.from_user.mention} là {balance:,} điểm 💰")
-        await bot.send_message(group_id2, f"👤 Số điểm của {message.from_user.mention} là {balance:,} điểm 💰")
+        #await bot.send_message(group_id2, f"👤 Số điểm của {message.from_user.mention} là {balance:,} điểm 💰")
 
 def loai_cau(total_score):
   return "⚫️" if 11 <= total_score <= 18 else "⚪️"
