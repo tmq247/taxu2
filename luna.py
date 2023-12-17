df7eefc97d1d0707d2715091492f45f88b584336
tmq247 <60788746+tmq247@users.noreply.github.com>
2023-12-17 15:42:36 +0700

    Update luna.py

--- luna.py
+++ luna.py
@@ -497,7 +497,8 @@ def top_diem(_, message):
         for line in lines:
             user_id, diem = line.split()
             topdiem = []
-            topdiem += [user_id], [diem]
+            topdiem += user_id
+            topdiem[user_id] += diem
             #diem = "/n".join(reversed(diem))
             top += f"""{topdiem}"""
         bot.send_message(chat_id, top)
