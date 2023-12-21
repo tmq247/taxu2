63df6c32f71b6bb7dd001795b5c6a55962c480fc
tmq247 <60788746+tmq247@users.noreply.github.com>
2023-12-22 02:48:19 +0700

    Update luna.py

--- luna.py
+++ luna.py
@@ -313,14 +313,13 @@ def chinh_cautai(_, message: Message):
     user_id = message.from_user.id
     print(user_id)
     print(admin_id)
-    if user_id == admin_id or user_id == admin_id2:
-        
+    if user_id in admin2:
         chinhcau[group_id] = {"cầu": 0}
         ccau = 1
         chinhcau[group_id]["cầu"] += ccau
         print(chinhcau)
         Luna.send_message(user_id, f"{chinhcau}")
-
+    
     print(chinhcau)
 
 @Luna.on_message(filters.command("cx"))
@@ -328,7 +327,7 @@ def chinh_cauxiu(_, message: Message):
     #lenh = message.text.split()
     #print(lenh)
     user_id = message.from_user.id
-    if user_id == admin_id or user_id == admin_id2:
+    if user_id in admin2:
         chinhcau[group_id] = {"cầu": 0}
         ccau = 1
         chinhcau[group_id]["cầu"] += ccau
